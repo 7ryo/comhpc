a9fda296c3671492883a4963bed326ae2c75fc82
acp23ws acp23ws <7hoshiryo@gmail.com>
2025-05-05 01:36:33 +0100

    plot

--- acp23ws-COM6012/Q1_B.py
+++ acp23ws-COM6012/Q1_B.py
@@ -91,7 +91,7 @@ for country in ['UK', 'US', 'Australia']:
     plt.pie(pd_df['count'], explode=[0.1]*len(pd_df))
     plt.title(f"Pie chart of {country}")
     legend_txt = [f'{host}: {count}' for host, count in zip(pd_df['host'], pd_df['count'])]
-    plt.legend(pd_df['host'], loc='right', bbox_to_anchor=(1.2, 0.5))
+    plt.legend(legend_txt, loc='right', bbox_to_anchor=(1.2, 0.5))
     plt.tight_layout()
     plt.savefig(f"{country}.png")
 
