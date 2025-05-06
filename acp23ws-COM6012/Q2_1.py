0c576c0d4e964e566359551a7cf9d884b00dee82
acp23ws acp23ws <7hoshiryo@gmail.com>
2025-05-06 13:21:07 +0100

    update

--- acp23ws-COM6012/Q2_1.py
+++ acp23ws-COM6012/Q2_1.py
@@ -69,14 +69,15 @@ onehot_df.show(5, False) # sparse display
 
 #onehot_df.show(5)
 
-onehot_df.printSchema()
+#onehot_df.printSchema()
 
 # 2. convert "readmitted" to binary
 #    >30 and <30: 1
 #             No: 0
-onehot_df = onehot_df.withColumn('readmitted', F.when(F.col('readmitted')=="NO", 0).otherwise(1))
-onehot_df.select('readmitted').show(5)
+onehot_df = onehot_df.withColumn('readmitted', F.when(F.col('readmitted').contains('NO'), 0.0).otherwise(1.0))
+print(onehot_df.select('readmitted').groupBy('readmitted').count())
 
+#onehot_df.printSchema()
 
 # 3. select numeric feature from either 
 # “time_in_hospital” or "num_lab_procedures”
@@ -119,24 +120,36 @@ print("RMSE = %g \n" % rmse)
 #    Eval: accuracy
 
 from pyspark.ml.classification import LogisticRegression
-
+#from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 # L2: elasticNetParam=0
 logit_l2 = LogisticRegression(featuresCol='onehot', labelCol='readmitted', \
-				        maxIter=50, regParam=0.01, elasticNetParam=0)
+				        maxIter=50, regParam=0.1, elasticNetParam=0)
 # with elasticNetParam
 logit_elastic = LogisticRegression(featuresCol='onehot', labelCol='readmitted', \
-				        maxIter=50, regParam=0.01, elasticNetParam=0.5)
+				        maxIter=50, regParam=0.1, elasticNetParam=0.5)
 
 model_l2 = logit_l2.fit(trainData)
-pred_l2 = model_l2.transfomr(testData)
+pred_l2 = model_l2.transform(testData)
+
+#check prediction output
+pred_l2.show(10)
 
 model_elastic = logit_elastic.fit(trainData)
-pred_elastic = model_elastic.transfomr(testData)
+pred_elastic = model_elastic.transform(testData)
+pred_elastic.show(10)
 
-evaluator_logit = RegressionEvaluator(labelCol='readmitted', predictionCol='prediction', metricName='accuracy')
+evaluator_logit = MulticlassClassificationEvaluator(labelCol='readmitted', predictionCol='prediction', metricName='accuracy')
 
 acc_l2 = evaluator_logit.evaluate(pred_l2)
-print("Accuracy of l2 is %i\n" % acc_l2)
+print(f"Accuracy of l2 is {acc_l2}\n")
+
+###manul check
+#correct = pred_l2.filter("readmitted = prediction").count()
+#total = pred_l2.count()
+#print("Manual Accuracy: ", correct / total)
+
+
 
 acc_elastic = evaluator_logit.evaluate(pred_elastic)
-print("Accuracy of w/ elastic is %i\n" % acc_elastic)
+print(f"Accuracy of w/ elastic is {acc_elastic}\n")
--- acp23ws-COM6012/Q3_1.py
+++ acp23ws-COM6012/Q3_1.py
@@ -8,7 +8,7 @@ spark = SparkSession.builder\
         .getOrCreate()
 
 sc = spark.sparkContext
-sc.setLogLevel("WARN")
+sc.setLogLevel("ERROR")
 
 # load data csv
 train_raw_DF = spark.read.csv('./Data/5xor_128bit/train_5xor_128dim.csv')
@@ -18,18 +18,62 @@ rand_seed = 66896
 
 # A. Small dataset
 # random select 1% of original data
-small_train_DF = spark.createDataFrame(train_raw_DF).sample(fraction=0.01, seed=rand_seed)
-small_test_DF = spark.createDataFrame(test_raw_DF).sample(fraction=0.01, seed=rand_seed)
+small_train_DF = train_raw_DF.sample(fraction=0.01, seed=rand_seed)
+small_test_DF = test_raw_DF.sample(fraction=0.01, seed=rand_seed)
 print("small dataset count:")
 print(small_train_DF.count())
 print(small_test_DF.count())
 
 ### class balancing?
 
-small_train_DF.printSchema()
+#small_train_DF.printSchema()
+
+small_train_DF = small_test_DF.withColumn('labels', F.when(F.col('_c128')==-1, 0.0).otherwise(1.0)).drop('_c128')
+small_test_DF = small_test_DF.withColumn('labels', F.when(F.col('_c128')==-1, 0.0).otherwise(1.0)).drop('_c128')
+
+# value types are string
+# convert them into double/int
+from pyspark.sql.types import StringType
+StringColumns = [x.name for x in small_train_DF.schema.fields if x.dataType == StringType()]
+for c in StringColumns:
+    small_train_DF = small_train_DF.withColumn(c, F.col(c).cast("double"))
+    small_test_DF = small_test_DF.withColumn(c, F.col(c).cast("double"))
+
+#small_train_DF.printSchema() # they are double now
+
+# vector assembler to concat all features -> one vector
+from pyspark.ml.feature import VectorAssembler
+feature_names = small_train_DF.columns
+num_features = len(feature_names)
+vecAssembler = VectorAssembler(inputCols = feature_names[:-1], outputCol = 'features') 
+testvecTrainingData = vecAssembler.transform(small_train_DF)
+testvecTrainingData.show(5)
 
 # pipeline
+from pyspark.ml import Pipeline
+from pyspark.ml.regression import RandomForestRegressor
+
+rf = RandomForestRegressor(labelCol="labels", featuresCol="features", maxDepth=5, numTrees=3, \
+                           featureSubsetStrategy = 'all', seed=123, bootstrap=False)
 # Random Forests
+
+stages = [vecAssembler, rf]
+pipeline = Pipeline(stages=stages)
+
+pipelineModel = pipeline.fit(small_train_DF)
+predictions = pipelineModel.transform(small_test_DF)
+
+predictions.select('features', 'labels', 'pediction').show(10)
+
+# Eval
+# classification accuracy
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator 
+eval_multi = MulticlassClassificationEvaluator(labelCol="labels", predictionCol="prediction", metricName="accuracy")
+accuracy = eval_multi.evaluate(predictions)
+print("Accuracy = %g " % accuracy)
+
+# area under the curve
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 # Gradient boosting 
 # and (shallow) Neural networks
